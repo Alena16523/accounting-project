fd220b75ed64501e5452ecf81ba7def077d025d7
Sahin <ismailsinansahin@gmail.com>
2022-12-23 10:48:42 +0300

    initial commit

---
+++ src/main/java/com/cydeo/javahedgehogsproject/JavaHedgehogsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.javahedgehogsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHedgehogsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHedgehogsProjectApplication.class, args);
+    }
+
+}
