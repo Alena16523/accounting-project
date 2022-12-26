c210963e63e9a5b550e64847c3fa5c3dd7777554
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-26 16:53:41 -0500

    JDZH 183 - Invoice entity is Created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/Invoice.java
@@ -0,0 +1,27 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "invoices")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Invoice extends BaseEntity{
+
+    private String invoiceNo;
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus invoiceStatus;
+    @Enumerated(EnumType.STRING)
+    private InvoiceType invoiceType;
+    private LocalDate date;
+    @ManyToOne
+    private ClientVendor clientVendor;
+    @ManyToOne
+    private Company company;
+
+}
