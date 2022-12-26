2cb3f8713f7d6908dce5c0893e38831ad0645a66
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-26 18:08:09 -0500

    JDZH 207 - RoleRepository is Created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.cydeo.javahedgehogsproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
