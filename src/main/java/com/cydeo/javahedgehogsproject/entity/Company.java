3ca5f280a23df84857cc745436f8445b15f64ff9
Sabina Omarova <sabomar170@gmail.com>
2022-12-27 12:31:06 -0500

    JDZH6-172 Company Entity is created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/Company.java
@@ -0,0 +1,32 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import com.cydeo.javahedgehogsproject.enums.CompanyStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "companies")
+public class Company extends BaseEntity {
+
+    @Column(unique = true)
+    private String title;
+    private String phone;
+    private String website;
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+    @OneToOne()
+    private Address address;
+
+
+
+
+
+
+
+}
