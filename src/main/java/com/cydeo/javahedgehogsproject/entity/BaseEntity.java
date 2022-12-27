e220389962c8d47969b624699b62d67a64df3eed
Esmira Mindrescu <e7meralda@gmail.com>
2022-12-27 11:17:11 -0500

    JDZH6-168-Base Entity class created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime LastUpdateDateTime;
+    @Column(nullable = false)
+    private Long lastUpdateUserId;
+    boolean isDeleted = false;
+
+}
