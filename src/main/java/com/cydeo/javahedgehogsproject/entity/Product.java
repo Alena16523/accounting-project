db8a788b9ea303dfd4e1858d14ab78d6318eebd0
marika <tsotsonavamarika@gmail.com>
2022-12-27 12:35:17 -0500

    JDZH6-178-179 Product entity class is created, ProductUnit enum is created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/entity/Product.java
@@ -0,0 +1,24 @@
+package com.cydeo.javahedgehogsproject.entity;
+
+import com.cydeo.javahedgehogsproject.enums.ProductUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
+    //@ManyToOne
+    //@JoinColumn(name = "category")
+    //private Category category;
+}
