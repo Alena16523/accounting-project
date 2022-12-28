cbe307ff85e77fad24ce6ce2eaebde7573f545c2
Esmira Mindrescu <e7meralda@gmail.com>
2022-12-28 13:48:49 -0500

    JDZH6-242-created client vendor dto

---
+++ src/main/java/com/cydeo/javahedgehogsproject/dto/ClientVendorDto.java
@@ -0,0 +1,20 @@
+package com.cydeo.javahedgehogsproject.dto;
+
+import com.cydeo.javahedgehogsproject.enums.ClientVendorType;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientVendorDto {
+
+    private Long id;
+    private String clientVendorName;
+    private String phone;
+    private String website;
+    private ClientVendorType clientVendorType;
+    private AddressDTO address;
+    private CompanyDTO company;
+}
