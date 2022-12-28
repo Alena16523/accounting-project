9c7b1a7894b72b9e272aa1bda3f213850df40f9a
Esmira Mindrescu <e7meralda@gmail.com>
2022-12-28 14:49:12 -0500

    JDZH6-240, 241-created client vendor service impl, creaed service interface

---
+++ src/main/java/com/cydeo/javahedgehogsproject/service/ClientVendorService.java
@@ -0,0 +1,8 @@
+package com.cydeo.javahedgehogsproject.service;
+
+import com.cydeo.javahedgehogsproject.dto.ClientVendorDto;
+
+public interface ClientVendorService {
+    ClientVendorDto findById (Long id);
+
+}
