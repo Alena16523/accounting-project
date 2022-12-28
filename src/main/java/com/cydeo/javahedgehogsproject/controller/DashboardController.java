a6755eafab69fcfdbdc6eb70ee970761cf70eb88
Sahin <ismailsinansahin@gmail.com>
2022-12-28 11:15:44 +0300

    all html files for CRUD operations are ready

---
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package com.cydeo.javahedgehogsproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String navigateToDashboard(Model model) throws Exception {
+        return "dashboard";
+    }
+
+}
