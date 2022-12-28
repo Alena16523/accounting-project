898ccf99573ccf0b3805a43d24119be3c9eaa819
Alena <alenaelhajassaad@yahoo.com>
2022-12-28 13:35:50 -0500

    JDZH6-271-272 created CategoryController class and CategoryDTOConverter class with convert method. Added findById method in CategoryService and implemented.

--- src/main/java/com/cydeo/javahedgehogsproject/service/CategoryService.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/CategoryService.java
@@ -1,7 +1,12 @@
 package com.cydeo.javahedgehogsproject.service;
 
 
+import com.cydeo.javahedgehogsproject.dto.CategoryDto;
+import com.cydeo.javahedgehogsproject.dto.RoleDto;
+
 public interface CategoryService {
+
+    CategoryDto findById(long id);
 
 
 }
--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CategoryServiceImpl.java
@@ -1,8 +1,28 @@
 package com.cydeo.javahedgehogsproject.service.implementation;
 
+import com.cydeo.javahedgehogsproject.dto.CategoryDto;
+import com.cydeo.javahedgehogsproject.dto.RoleDto;
+import com.cydeo.javahedgehogsproject.entity.Category;
+import com.cydeo.javahedgehogsproject.mapper.MapperUtil;
+import com.cydeo.javahedgehogsproject.repository.CategoryRepository;
 import com.cydeo.javahedgehogsproject.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+    @Override
+    public CategoryDto findById(long id) {
+        Category category = categoryRepository.findById(id).get();
+        return mapperUtil.convert(category,new CategoryDto());
+    }
 }
