69699d2a1f95d73397340fe23e354957754fcc12
Sabina Omarova <sabomar170@gmail.com>
2022-12-28 13:43:33 -0500

    JDZH6-257 findById method in ProductService is created

--- src/main/java/com/cydeo/javahedgehogsproject/service/ProductService.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/ProductService.java
@@ -2,4 +2,5 @@ package com.cydeo.javahedgehogsproject.service;
 
 public interface ProductService {
 
+    ProductDTO findById(long id);
 }
--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/ProductServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/ProductServiceImpl.java
@@ -1,8 +1,25 @@
 package com.cydeo.javahedgehogsproject.service.implementation;
 
+import com.cydeo.javahedgehogsproject.entity.Product;
+import com.cydeo.javahedgehogsproject.mapper.MapperUtil;
+import com.cydeo.javahedgehogsproject.repository.ProductRepository;
 import com.cydeo.javahedgehogsproject.service.ProductService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final MapperUtil mapperUtil;
+
+    public ProductServiceImpl(ProductRepository productRepository, MapperUtil mapperUtil) {
+        this.productRepository = productRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public ProductDTO findById(long id) {
+        Product product = productRepository.findById(id).get();
+        return mapperUtil.convert(product,new ProductDTO());
+    }
 }
