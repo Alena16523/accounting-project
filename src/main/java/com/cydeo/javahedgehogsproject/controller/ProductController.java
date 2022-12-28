ff6c8007b162083985a86dfc2950bd45febf88c0
Sabina Omarova <sabomar170@gmail.com>
2022-12-28 10:28:52 -0500

    JDZH6-258 ProductController is created

--- src/main/java/com/cydeo/javahedgehogsproject/controller/ProductController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/ProductController.java
@@ -1,9 +1,10 @@
 package com.cydeo.javahedgehogsproject.controller;
 
-import org.springframework.stereotype.Component;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+@Controller
 @RequestMapping("/products")
 public class ProductController {
 
