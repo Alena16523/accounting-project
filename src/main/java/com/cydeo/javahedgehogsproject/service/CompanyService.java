af54635c808ccba9c4c91cdcd6dc084505ce80bf
mizgin <mizgincvk@gmail.com>
2022-12-28 14:57:25 -0500

    JDZH6-243 - added findById method

--- src/main/java/com/cydeo/javahedgehogsproject/service/CompanyService.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/CompanyService.java
@@ -1,4 +1,9 @@
 package com.cydeo.javahedgehogsproject.service;
 
+import com.cydeo.javahedgehogsproject.dto.CompanyDTO;
+
 public interface CompanyService {
+
+    CompanyDTO findById(Long id);
+
 }
