8c7b3f0e6110cbd31d7c430b908588009bf704d9
mizgin <mizgincvk@gmail.com>
2022-12-29 13:40:24 -0500

    created get category list and implemented getLoggedInCompany in SecurityServiceImpl

--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CategoryServiceImpl.java
@@ -5,13 +5,13 @@ import com.cydeo.javahedgehogsproject.dto.CompanyDto;
 import com.cydeo.javahedgehogsproject.dto.UserDto;
 import com.cydeo.javahedgehogsproject.entity.Category;
 import com.cydeo.javahedgehogsproject.entity.Company;
-import com.cydeo.javahedgehogsproject.entity.User;
 import com.cydeo.javahedgehogsproject.mapper.MapperUtil;
 import com.cydeo.javahedgehogsproject.repository.CategoryRepository;
-import com.cydeo.javahedgehogsproject.repository.UserRepository;
 import com.cydeo.javahedgehogsproject.service.CategoryService;
 import com.cydeo.javahedgehogsproject.service.CompanyService;
 import com.cydeo.javahedgehogsproject.service.SecurityService;
+import com.cydeo.javahedgehogsproject.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,15 +22,15 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final MapperUtil mapperUtil;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final SecurityService securityService;
     private final CompanyService companyService;
 
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil, UserRepository userRepository, SecurityService securityService, CompanyService companyService) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil, UserService userService, SecurityService securityService, CompanyService companyService) {
         this.categoryRepository = categoryRepository;
         this.mapperUtil = mapperUtil;
-        this.userRepository = userRepository;
+        this.userService = userService;
         this.securityService = securityService;
         this.companyService = companyService;
     }
@@ -39,21 +39,24 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public CategoryDto findById(long id) {
         Category category = categoryRepository.findById(id).get();
-        return mapperUtil.convert(category,new CategoryDto());
+        return mapperUtil.convert(category, new CategoryDto());
     }
 
     @Override
     public List<CategoryDto> listAllCategoriesByUser() {
         //getting all categories from DB
-        UserDto loggedInUser=securityService.getLoggedInUser();
-        User loggedInEntityUser=mapperUtil.convert(loggedInUser, new User());
 
-       CompanyDto companyDto= companyService.findById(loggedInEntityUser.getCompany().getId());
-       Company company=mapperUtil.convert(companyDto, new Company());
+//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//        UserDto userDto = userService.findByUsername(username);
+//        CompanyDto companyDto = companyService.findById(userDto.getCompany().getId());
+//        Company company = mapperUtil.convert(companyDto, new Company());
 
-        List<Category> listOfCategories=categoryRepository.findAllByCompanyId(company.getId());
+        CompanyDto companyDto = securityService.getLoggedInCompany();
+        Company company = mapperUtil.convert(companyDto, new Company());
+
+        List<Category> listOfCategories = categoryRepository.findAllByCompanyId(company.getId());
 
         //converting one by one category to DTO and returning List
-        return listOfCategories.stream().map(category->mapperUtil.convert(category, new CategoryDto())).collect(Collectors.toList());
+        return listOfCategories.stream().map(category -> mapperUtil.convert(category, new CategoryDto())).collect(Collectors.toList());
     }
 }
