d685794edfae0f81fcc3da1eaf4e27c0a8c1ed35
taniusa90 <galea_tanea@mail.com>
2022-12-29 15:44:42 -0600

    JDZH6-259-getMapping to listAll the products is created

--- src/main/java/com/cydeo/javahedgehogsproject/repository/ProductRepository.java
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/ProductRepository.java
@@ -1,14 +1,19 @@
 package com.cydeo.javahedgehogsproject.repository;
 
+import com.cydeo.javahedgehogsproject.entity.Company;
 import com.cydeo.javahedgehogsproject.entity.Product;
 import com.cydeo.javahedgehogsproject.enums.ProductUnit;
+import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
 
 
 }
