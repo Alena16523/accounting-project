e947d134c1a964a5bc56508f93096e346b256926
mizgin <mizgincvk@gmail.com>
2022-12-29 11:26:04 -0500

    JDZH6-220,221,222 - Company CRUD

--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CompanyServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/CompanyServiceImpl.java
@@ -9,7 +9,6 @@ import com.cydeo.javahedgehogsproject.service.CompanyService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
