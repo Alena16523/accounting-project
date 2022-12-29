b42278c41315b5acfa64c70d2d85f53a7ac5a2ba
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-29 07:01:09 -0500

    JDZH - 234 User controller Method added

--- src/main/java/com/cydeo/javahedgehogsproject/controller/UserController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.cydeo.javahedgehogsproject.controller;
 
+import com.cydeo.javahedgehogsproject.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model){
+        model.addAttribute("users", userService.findAllSortedByCompanyAndRoles());
+
+        return "fragments/left-sidebar";
+    }
 }
