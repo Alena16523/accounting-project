8428add44cfef54f958f51248a65e2713c4cd7fa
mizgin <mizgincvk@gmail.com>
2022-12-30 00:47:27 -0500

    JDZH6-223,221 added validation for Company

--- src/main/java/com/cydeo/javahedgehogsproject/dto/AddressDto.java
+++ src/main/java/com/cydeo/javahedgehogsproject/dto/AddressDto.java
@@ -4,17 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressDto {
 
     private Long id;
+    @NotBlank
+    @Size(max = 100, min = 2)
     private String addressLine1;
+    @Size(max = 100)
     private String addressLine2;
+    @NotBlank
+    @Size(max = 50, min = 2)
     private String city;
+    @NotBlank
+    @Size(max = 50, min = 2)
     private String state;
+    @NotBlank
+    @Size(max = 50, min = 2)
     private String country;
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{5}(?:-[0-9]{4})?$")
     private String zipCode;
 
 }
+//@Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$") // 8 to 11 digits
--- src/main/java/com/cydeo/javahedgehogsproject/dto/CompanyDto.java
+++ src/main/java/com/cydeo/javahedgehogsproject/dto/CompanyDto.java
@@ -3,6 +3,11 @@ package com.cydeo.javahedgehogsproject.dto;
 import com.cydeo.javahedgehogsproject.enums.CompanyStatus;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
@@ -11,9 +16,16 @@ import lombok.*;
 public class CompanyDto {
 
     private Long id;
+    @NotBlank
+    @Size(max = 100, min = 2)
     private String title;
+    @NotBlank
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$") // 8 to 11 digits
     private String phone;
+    @NotBlank
+    @Pattern(regexp = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
     private String website;
+    @NotNull
     private AddressDto address;
     private CompanyStatus companyStatus;
 
