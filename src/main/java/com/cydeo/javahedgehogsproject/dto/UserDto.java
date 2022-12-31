ebe2961e44566baeed38fd70c28ab704339d6ced
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-31 08:38:01 -0500

    Revert "JDZH - 234 User Validation"
    This reverts commit 939942a6dc9f951ed6cf2583a023ac90ede30e8e.

--- src/main/java/com/cydeo/javahedgehogsproject/dto/UserDto.java
+++ src/main/java/com/cydeo/javahedgehogsproject/dto/UserDto.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.*;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,26 +12,13 @@ import javax.validation.constraints.*;
 public class UserDto {
 
     private Long id;
-    @NotBlank
-    @Size(max = 15, min = 2)
     private String firstname;
-    @NotBlank
-    @Size(max = 15, min = 2)
     private String lastname;
-    @NotBlank
-    @Email
     private String username;
-    @NotBlank
-    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
-    @NotNull
     private String confirmPassword;
-    @NotBlank
-    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$")
     private String phone;
-    @NotNull
     private RoleDto role;
-    @NotNull
     private CompanyDto company;
     private Boolean isOnlyAdmin;
 
