d3245690fa8f90f8c38ebed143fb05c698bddfb7
Esmira Mindrescu <e7meralda@gmail.com>
2023-01-03 13:01:29 -0500

    JDZH6-284 added @Service, @Repository annotations

--- src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceProductRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.javahedgehogsproject.repository;
 
 import com.cydeo.javahedgehogsproject.entity.InvoiceProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
 
 }
--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/InvoiceProductServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/InvoiceProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cydeo.javahedgehogsproject.service.implementation;
 
 import com.cydeo.javahedgehogsproject.service.InvoiceProductService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvoiceProductServiceImpl implements InvoiceProductService {
 }
