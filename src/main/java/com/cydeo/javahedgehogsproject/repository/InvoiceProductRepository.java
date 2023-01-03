727091b485ec1f706449aa00d4fc100298cd77ce
Esmira Mindrescu <e7meralda@gmail.com>
2023-01-03 11:35:16 -0500

    JDZH6-284 (3 tickets): created InvoiceProductRepository interface, InvoiceProductService interface, InvoiceProductServiceImpl

---
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceProductRepository.java
@@ -0,0 +1,8 @@
+package com.cydeo.javahedgehogsproject.repository;
+
+import com.cydeo.javahedgehogsproject.entity.InvoiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
+
+}
---
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/InvoiceProductServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cydeo.javahedgehogsproject.service.implementation;
+
+import com.cydeo.javahedgehogsproject.service.InvoiceProductService;
+
+public class InvoiceProductServiceImpl implements InvoiceProductService {
+}
