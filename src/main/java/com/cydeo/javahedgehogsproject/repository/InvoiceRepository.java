209eedbfdac6f2d6da469fd01e72b502afe3ad28
marika <tsotsonavamarika@gmail.com>
2023-01-03 11:09:06 -0500

    JDZH6_286_289_288_287_285 invoiceDto, invoiceRepository, invoiceConverter, service and service implementation created

---
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package com.cydeo.javahedgehogsproject.repository;
+
+import com.cydeo.javahedgehogsproject.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
+
+
+
+}
---
+++ src/main/java/com/cydeo/javahedgehogsproject/service/InvoiceService.java
@@ -0,0 +1,9 @@
+package com.cydeo.javahedgehogsproject.service;
+
+import com.cydeo.javahedgehogsproject.dto.CategoryDto;
+import com.cydeo.javahedgehogsproject.dto.InvoiceDto;
+
+public interface InvoiceService {
+
+    InvoiceDto findById(long id);
+}
