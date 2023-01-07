1165c063c2d10461e65e8cd6c18aa99d31bea507
Alena <alenaelhajassaad@yahoo.com>
2023-01-07 11:33:54 -0500

    create InvoicePurchaseInvoice

--- src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceRepository.java
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceRepository.java
@@ -1,10 +1,16 @@
 package com.cydeo.javahedgehogsproject.repository;
 
+import com.cydeo.javahedgehogsproject.entity.Company;
 import com.cydeo.javahedgehogsproject.entity.Invoice;
+import com.cydeo.javahedgehogsproject.enums.InvoiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    Long countAllByInvoiceTypeAndCompanyId(InvoiceType invoiceType, Long companyId);
+
 
 }
