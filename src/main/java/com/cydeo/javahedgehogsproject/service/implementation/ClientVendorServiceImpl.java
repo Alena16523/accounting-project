12f61ead358f274fe433655d87fdc9fe06855d24
taniusa90 <galea_tanea@mail.com>
2023-01-07 12:41:20 -0600

    JDZH6-298-Invoice CRUD

--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/ClientVendorServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/ClientVendorServiceImpl.java
@@ -92,6 +92,8 @@ public class ClientVendorServiceImpl implements ClientVendorService {
         clientVendorRepository.save(clientVendor);
     }
 
+
+
     @Override
     public boolean checkIfThereIsAnyClientVendorWithSameNameAndType(String name, ClientVendorType type) {
         return clientVendorRepository.findAllByCompany(mapperUtil.convert(securityService.getLoggedInCompany(), new Company()))
