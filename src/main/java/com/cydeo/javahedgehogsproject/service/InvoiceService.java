4b4abd32eae0077a673837539528d92119701570
Sabina Omarova <sabomar170@gmail.com>
2023-01-07 14:23:49 -0500

    JDZH6-304

--- src/main/java/com/cydeo/javahedgehogsproject/service/InvoiceService.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/InvoiceService.java
@@ -8,18 +8,14 @@ import java.util.List;
 public interface InvoiceService {
 
     InvoiceDto findById(long id);
-    List<InvoiceDto> findAllInvoice(InvoiceType invoiceType);
 
+    List<InvoiceDto> findAllInvoice(InvoiceType invoiceType);
 
     InvoiceDto getNewInvoice(InvoiceType invoiceType);
 
-     String InvoiceNo(InvoiceType invoiceType, Long companyId);
-
+    String InvoiceNo(InvoiceType invoiceType, Long companyId);
 
     void delete(Long id);
 
-  //  List<InvoiceDto> findAllInvoicesByInvoiceType(InvoiceType invoiceType);
-
-    List<InvoiceDto> findAllInvoice(InvoiceType invoiceType);
 
 }
