3988f2ff041f3301677f731916f080252f5cc9f2
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-08 09:49:13 -0500

    JDZH - 294 invoice update

--- src/main/java/com/cydeo/javahedgehogsproject/controller/PurchaseInvoiceController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/PurchaseInvoiceController.java
@@ -1,12 +1,15 @@
 package com.cydeo.javahedgehogsproject.controller;
 
 import com.cydeo.javahedgehogsproject.dto.InvoiceDto;
+import com.cydeo.javahedgehogsproject.dto.InvoiceProductDto;
 import com.cydeo.javahedgehogsproject.entity.Invoice;
 import com.cydeo.javahedgehogsproject.enums.InvoiceType;
 import com.cydeo.javahedgehogsproject.mapper.MapperUtil;
 import com.cydeo.javahedgehogsproject.repository.InvoiceRepository;
 import com.cydeo.javahedgehogsproject.service.ClientVendorService;
+import com.cydeo.javahedgehogsproject.service.InvoiceProductService;
 import com.cydeo.javahedgehogsproject.service.InvoiceService;
+import com.cydeo.javahedgehogsproject.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +26,16 @@ public class PurchaseInvoiceController {
     private final ClientVendorService clientVendorService;
     private final InvoiceRepository invoiceRepository;
     private final MapperUtil mapperUtil;
+    private final ProductService productService;
+    private final InvoiceProductService invoiceProductService;
 
-    public PurchaseInvoiceController(InvoiceService invoiceService, ClientVendorService clientVendorService, InvoiceRepository invoiceRepository, MapperUtil mapperUtil) {
+    public PurchaseInvoiceController(InvoiceService invoiceService, ClientVendorService clientVendorService, InvoiceRepository invoiceRepository, MapperUtil mapperUtil, ProductService productService, InvoiceProductService invoiceProductService) {
         this.invoiceService = invoiceService;
         this.clientVendorService = clientVendorService;
         this.invoiceRepository = invoiceRepository;
         this.mapperUtil = mapperUtil;
+        this.productService = productService;
+        this.invoiceProductService = invoiceProductService;
     }
 
     @GetMapping("/list")
@@ -57,5 +64,37 @@ public class PurchaseInvoiceController {
             invoiceRepository.save(purchaseInvoice);
         return "redirect:/purchaseInvoices/update/";
     }
+    @GetMapping("/update/{invoiceId}")
+    public String editInvoice(@PathVariable("invoiceId") Long id, Model model) {
 
+        model.addAttribute("invoice", invoiceService.findById(id));
+        model.addAttribute("vendors", clientVendorService.findAllVendors());
+
+        model.addAttribute("newInvoiceProduct", new InvoiceProductDto());
+        model.addAttribute("products", productService.listAllProducts());
+
+        model.addAttribute("invoiceProducts", invoiceProductService.findAllById(id));
+
+        return "invoice/purchase-invoice-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateInvoice(@ModelAttribute("invoice") InvoiceDto invoice, Model model) {
+        model.addAttribute("vendors", clientVendorService.findAllVendors());
+        invoiceService.update(invoice);
+        return "redirect:/purchaseInvoices/list";
+    }
+
+    @PostMapping("/addInvoiceProduct/{invoiceId}")
+    public String createInvoiceProduct(@PathVariable("invoiceId") Long invoiceId, @ModelAttribute("newInvoiceProduct") InvoiceProductDto invoiceProduct, Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        invoiceProductService.saveByInvoiceId(invoiceProduct, invoiceId);
+        return "redirect:/purchaseInvoices/update/" + invoiceId;
+    }
+
+    @GetMapping("/removeInvoiceProduct/{invoiceId}/{productId}")
+    public String removeProduct(@PathVariable Long productId, @PathVariable Long invoiceId) {
+        invoiceProductService.delete(productId);
+        return "redirect:/purchaseInvoices/update/" + invoiceId;
+    }
 }
