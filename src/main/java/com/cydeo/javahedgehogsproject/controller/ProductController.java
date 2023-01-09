99d9162bc116dfdd570cc9ead878708d73b96444
mizgin <mizgincvk@gmail.com>
2023-01-09 12:46:28 -0500

    JDZH6-305 reformatted

--- src/main/java/com/cydeo/javahedgehogsproject/controller/ProductController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/ProductController.java
@@ -1,7 +1,5 @@
 package com.cydeo.javahedgehogsproject.controller;
 
-
-
 import com.cydeo.javahedgehogsproject.dto.ProductDto;
 import com.cydeo.javahedgehogsproject.enums.ProductUnit;
 import com.cydeo.javahedgehogsproject.service.CategoryService;
@@ -12,7 +10,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
 
 @Controller
 @RequestMapping("/products")
@@ -26,21 +23,18 @@ public class ProductController {
         this.categoryService = categoryService;
     }
 
-
-
     @GetMapping("/list")
-    public String listAllProducts(Model model){
-        model.addAttribute("products",productService.listAllProducts());
+    public String listAllProducts(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
         return "/product/product-list";
     }
 
     @GetMapping("/create")
     public String createProduct(Model model) {
         model.addAttribute("newProduct", new ProductDto());
-        model.addAttribute("categories", categoryService.retrieveCategoryByCompany() );
+        model.addAttribute("categories", categoryService.retrieveCategoryByCompany());
         model.addAttribute("productUnits", ProductUnit.values());
         model.addAttribute("products", productService.listAllProducts());
-
 
         return "/product/product-create";
     }
@@ -48,7 +42,7 @@ public class ProductController {
     @PostMapping("/create")
     public String insertProduct(@Valid @ModelAttribute("newProduct") ProductDto newProduct, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors() || productService.isNameExist(newProduct.getName())) {
-            if (productService.isNameExist(newProduct.getName())){
+            if (productService.isNameExist(newProduct.getName())) {
                 bindingResult.rejectValue("name", " ", "This Product Name already exists");
             }
             model.addAttribute("categories", categoryService.retrieveCategoryByCompany());
@@ -60,38 +54,31 @@ public class ProductController {
         return "redirect:/products/list";
     }
 
-
     @GetMapping("update/{id}")
-    public String editProduct(@PathVariable ("id")Long id,Model model){
-        model.addAttribute("product",productService.findById(id));
-        model.addAttribute("categories",categoryService.retrieveCategoryByCompany());
+    public String editProduct(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        model.addAttribute("categories", categoryService.retrieveCategoryByCompany());
         model.addAttribute("productUnits", ProductUnit.values());
         return "/product/product-update";
     }
 
     @PostMapping("/update/{id}")
     public String updateProduct(@Valid @ModelAttribute("product") ProductDto product, BindingResult bindingResult, Model model) {
-
         if (bindingResult.hasErrors()) {
             product.getCategory().setHasProduct(true);
             model.addAttribute("categories", categoryService.retrieveCategoryByCompany());
             model.addAttribute("productUnits", ProductUnit.values());
-        return "/product/product-update";
+            return "/product/product-update";
         }
         productService.update(product);
         return "redirect:/products/list";
     }
 
-
-
     @GetMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable("id")Long id){
+    public String deleteProduct(@PathVariable("id") Long id) {
         productService.delete(id);
         return "redirect:/products/list";
-
-
-
     }
 
-    }
+}
 
