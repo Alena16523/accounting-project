6e05b3e95187a4db32cd353ed5290fb50831941e
mizgin <mizgincvk@gmail.com>
2023-01-09 11:57:04 -0500

    JDZH6-305 created derived query to get all invoiceProducts based on invoice status and current company

--- src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/javahedgehogsproject/repository/InvoiceProductRepository.java
@@ -2,6 +2,7 @@ package com.cydeo.javahedgehogsproject.repository;
 
 import com.cydeo.javahedgehogsproject.entity.Company;
 import com.cydeo.javahedgehogsproject.entity.InvoiceProduct;
+import com.cydeo.javahedgehogsproject.enums.InvoiceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +14,7 @@ public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, 
     List<InvoiceProduct> findAllByInvoiceId(Long invoice_id);
 
     List<InvoiceProduct> findAllByInvoice_CompanyAndInvoiceId(Company company, Long id);
+
+    List<InvoiceProduct> findAllByInvoice_InvoiceStatusAndInvoice_CompanyOrderByInvoice_DateDesc(InvoiceStatus invoiceStatus, Company company);
 
 }
