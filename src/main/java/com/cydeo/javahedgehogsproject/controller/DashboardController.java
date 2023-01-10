268f9aa1f7972d24a925b3462d19140dfccf29e9
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-10 12:35:42 -0500

    JDZH - 322 Dashboard CurrencyExchange

--- src/main/java/com/cydeo/javahedgehogsproject/controller/DashboardController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/DashboardController.java
@@ -1,28 +1,19 @@
 package com.cydeo.javahedgehogsproject.controller;
 
 import com.cydeo.javahedgehogsproject.enums.InvoiceStatus;
-import com.cydeo.javahedgehogsproject.enums.InvoiceType;
 import com.cydeo.javahedgehogsproject.service.InvoiceService;
-import org.springframework.stereotype.Controller;
-import com.cydeo.javahedgehogsproject.client.CurrencyClient;
 import com.cydeo.javahedgehogsproject.service.DashboardService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DashboardController {
     private final DashboardService dashboardService;
-
-    public DashboardController(DashboardService dashboardService) {
-
-        this.dashboardService = dashboardService;
-    }
-
-
     private final InvoiceService invoiceService;
 
-    public DashboardController(InvoiceService invoiceService) {
+    public DashboardController(DashboardService dashboardService, InvoiceService invoiceService) {
+        this.dashboardService = dashboardService;
         this.invoiceService = invoiceService;
     }
 
@@ -33,6 +24,7 @@ public class DashboardController {
         model.addAttribute("invoices", invoiceService.findAllApprovedInvoice(InvoiceStatus.APPROVED));
 
         model.addAttribute("currency", dashboardService.getCurrency());
+
         return "dashboard";
     }
 
