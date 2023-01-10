0ac8bd7878491ed299291e98de22873ef4a4c619
Alena <alenaelhajassaad@yahoo.com>
2023-01-10 16:04:07 -0500

    created dashboard functionality with available balances

--- src/main/java/com/cydeo/javahedgehogsproject/controller/DashboardController.java
+++ src/main/java/com/cydeo/javahedgehogsproject/controller/DashboardController.java
@@ -1,14 +1,15 @@
 package com.cydeo.javahedgehogsproject.controller;
 
 import com.cydeo.javahedgehogsproject.enums.InvoiceStatus;
-import com.cydeo.javahedgehogsproject.service.InvoiceService;
 import com.cydeo.javahedgehogsproject.service.DashboardService;
+import com.cydeo.javahedgehogsproject.service.InvoiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+
     private final DashboardService dashboardService;
     private final InvoiceService invoiceService;
 
@@ -19,10 +20,10 @@ public class DashboardController {
 
 
     @GetMapping("/dashboard")
-    public String navigateToDashboard(Model model) throws Exception {
+    public String navigateToDashboard(Model model) throws Exception{
 
+        model.addAttribute("dashboardNumbers",dashboardService.dashboardNumbers() );
         model.addAttribute("invoices", invoiceService.findAllApprovedInvoice(InvoiceStatus.APPROVED));
-
         model.addAttribute("currency", dashboardService.getCurrency());
 
         return "dashboard";
--- src/main/java/com/cydeo/javahedgehogsproject/service/implementation/DashboardServiceImpl.java
+++ src/main/java/com/cydeo/javahedgehogsproject/service/implementation/DashboardServiceImpl.java
@@ -1,19 +1,34 @@
 package com.cydeo.javahedgehogsproject.service.implementation;
 
+import com.cydeo.javahedgehogsproject.dto.InvoiceDto;
+import com.cydeo.javahedgehogsproject.enums.InvoiceStatus;
+import com.cydeo.javahedgehogsproject.enums.InvoiceType;
+import com.cydeo.javahedgehogsproject.client.CurrencyClient;
+import com.cydeo.javahedgehogsproject.dto.Currency;
+import com.cydeo.javahedgehogsproject.dto.Usd;
 import com.cydeo.javahedgehogsproject.client.CurrencyClient;
 import com.cydeo.javahedgehogsproject.dto.Currency;
 import com.cydeo.javahedgehogsproject.dto.Usd;
 import com.cydeo.javahedgehogsproject.service.DashboardService;
+import com.cydeo.javahedgehogsproject.service.InvoiceService;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
+
+    private final InvoiceService invoiceService;
     private final CurrencyClient currencyClient;
 
-    public DashboardServiceImpl(CurrencyClient currencyClient) {
+    public DashboardServiceImpl(InvoiceService invoiceService, CurrencyClient currencyClient) {
+
+        this.invoiceService = invoiceService;
         this.currencyClient = currencyClient;
     }
 
@@ -22,4 +37,43 @@ public class DashboardServiceImpl implements DashboardService {
         Currency currencies = currencyClient.getCurrencies();
         return currencies.getUsd();
     }
+
+    @Override
+    public Map<String, BigDecimal> dashboardNumbers(){
+
+        Map<String, BigDecimal> totalNumbersToDisplay=new HashMap<>();
+        totalNumbersToDisplay.put("totalCost", calculateTotalCost());
+        totalNumbersToDisplay.put("totalSales", calculateTotalSales());
+        totalNumbersToDisplay.put("Profit / Loss", calculateProfitLoss());
+
+        return totalNumbersToDisplay;
+    }
+
+
+    @Override
+    public BigDecimal calculateTotalCost() {
+        BigDecimal total=invoiceService.findAllInvoice(InvoiceType.PURCHASE).stream()
+                .filter(invoiceDto -> invoiceDto.getInvoiceStatus().equals(InvoiceStatus.APPROVED))
+                .map(InvoiceDto::getTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return total;
+    }
+
+    @Override
+    public BigDecimal calculateTotalSales() {
+        BigDecimal total=invoiceService.findAllInvoice(InvoiceType.SALES).stream()
+                .filter(invoiceDto -> invoiceDto.getInvoiceStatus().equals(InvoiceStatus.APPROVED))
+                .map(InvoiceDto::getTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return total;
+    }
+
+    @Override
+    public BigDecimal calculateProfitLoss() {
+
+        BigDecimal profitLoss=calculateTotalSales().subtract(calculateTotalCost());
+
+        return profitLoss;
+    }
+
 }
